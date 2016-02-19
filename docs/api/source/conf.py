7852d3b1cb25948ba228845dfef266159f82bd88
James Booth <jabooth@gmail.com>
2016-02-19 11:25:21 +0000

    move docs to docs/api

--- docs/api/source/conf.py
+++ docs/api/source/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 if not on_rtd:
   # Add the menpo root so we can grab the version
-  sys.path.insert(0, os.path.abspath('../../'))
+  sys.path.insert(0, os.path.abspath('../../../'))
 
 import menpo
 
